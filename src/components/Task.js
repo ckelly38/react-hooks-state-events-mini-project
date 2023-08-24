5326c083c1593f48e6009d9f55f6663da26a7efa
Christopher Hugh Kelly <christopher.kelly1997@gmail.com>
2023-08-24 14:19:45 -0600

    lists the tasks

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,10 +1,23 @@
 import React from "react";
 
-function Task() {
+function Task({text, catg}) {
+  if (text === undefined || text === null || catg === undefined || catg === null)
+  {
+    throw new Error("the text and the category for the new task must be defined and not null!");
+  }
+  else
+  {
+    if (text.length < 1 || catg.length < 1)
+    {
+      throw new Error("the text and the category for the new task must not be empty!");
+    }
+    //else;//do nothing
+  }
+
   return (
     <div className="task">
-      <div className="label">CATEGORY HERE</div>
-      <div className="text">TEXT HERE</div>
+      <div className="label">{catg}</div>
+      <div className="text">{text}</div>
       <button className="delete">X</button>
     </div>
   );
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,9 +1,16 @@
 import React from "react";
+import Task from "./Task";
+import { v4 as uuid } from "uuid";
 
-function TaskList() {
+function TaskList({mytasks}) {
+  let mytasksarr = mytasks.map((mtask) => {
+    return ( <Task key={uuid()} text={mtask.text} catg={mtask.category}/> );
+  });
+
   return (
     <div className="tasks">
       {/* display a list of tasks using Task component */}
+      {mytasksarr}
     </div>
   );
 }
